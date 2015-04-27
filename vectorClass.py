1faa5531a04866e183536b3fff69674034cfd7d9
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-04-27 22:31:53 +0200

    Update vectorClass.py

--- vectorClass.py
+++ vectorClass.py
@@ -1,49 +1,41 @@
-import math
-
 class Vector(object):
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
     def __str__(self):
-        return 'Vector object'
+        return 'Vector(' + str(self.x) +', ' + str(self.y) + ')'
 
-    #adds given vector to this vector and returns a new vector
     def add(self, v):
         self.v = v
-        return Vector(self.x + self.v.x, self.y + self.v.y)
+        self.x += self.v.x
+        self.y += self.v.y
 
-    #subtracts given vector from this vector and returns a new vector
     def sub(self, v):
         self.v = v
-        return Vector(self.x - self.v.x, self.y - self.v.y)
+        self.x -= self.v.x
+        self.y -= self.v.y
 
-    #returns the magnitude
     def mag(self):
         return math.sqrt(self.x**2 + self.y**2)
 
-    #normalizes the vector
     def norm(self):
         self.t_mag = self.mag()
         self.x /= self.t_mag
         self.y /= self.t_mag
 
-    #sets new magnitude
     def mult(self, m):
         self.m = m
         self.x *= self.m
         self.y *= self.m
 
-    #returns the dot product of given vector with this vector
     def dot(self, v):
         self.v = v
         return (self.x * self.v.x) + (self.y * self.v.y)
 
-    #returns the angle between given vector and this vector in radians
     def angle(self, v):
         self.v = v
         return math.acos(self.dot(self.v) / (self.mag() * self.v.mag()))
 
-    #returns the angle in radions
     def heading(self):
         return math.atan(self.y / self.x)
