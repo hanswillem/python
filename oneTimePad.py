eaca90ab84241c28f331c5c0e7c5ae3e4fcdaec1
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2015-05-16 14:28:06 +0200

    Update oneTimePad.py

--- oneTimePad.py
+++ oneTimePad.py
@@ -3,14 +3,12 @@
 m = 'Secret'
 k = [0,3,6,1,2,5]
 
-
 def encrypt(m, k):
     c = []
     for i in range(len(m)):
         c.append((ord(m[i]) + k[i]) % 123)
     return c
     
-
 def decrypt(c, k):
     m = []
     for i in range(len(c)):
