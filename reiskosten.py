37e9348db7aaf0799131383445c3d2867497095d
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-02-05 16:47:27 +0100

    Update reiskosten.py

--- reiskosten.py
+++ reiskosten.py
@@ -5,15 +5,16 @@ import calendar
 f = open('transacties 01.csv', 'rw')
 count = 0
 total = 0
-transitRecorder = []
+validTransits = []
+invalidTransits = []
 
 stops = ['Zuiderzeeweg', 'Centraal Station', 'Amsterdam Centraal', 'Ede-Wageningen']
 
 for row in f:
     if count > 0:
-
         currentTotal = total
 
+        #get date and day of the week
         dateString = row.split(';')[0]
         dateString = dateString[1:-1]
         year = int(dateString[-4:])
@@ -22,38 +23,41 @@ for row in f:
         theDate = date(year, month, day)
         dayOfTheWeek = calendar.day_name[theDate.weekday()]
 
+        #get to and from stops
         fromStop = row.split(';')[2]
         fromStop = fromStop[1:-1]
-
         toStop = row.split(';')[4]
         toStop = toStop[1:-1]
 
+        #get cost
         costString = row.split(';')[5]
-
         costString = costString[1:-1]
         cost = float (costString.split(',')[0] + '.' + costString.split(',')[1])
 
+        #find home-work transits
         if dayOfTheWeek != 'Saturday' and dayOfTheWeek != 'Sunday':
+            if fromStop in stops and toStop in stops:
+                total += cost
+                validTransits.append(dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR ' + costString)
 
-            if fromStop == stops[2] and toStop == stops[3]:
-                total += cost
-                transitRecorder.append(row)
-                print dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR §' + costString
-            if fromStop == stops[3] and toStop == stops[2]:
-                total += cost
-                transitRecorder.append(row)
-                print dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR §' + costString
-            if fromStop == stops[0] and toStop == stops[1]:
-                total += cost
-                transitRecorder.append(row)
-                print dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR §' + costString
-            if fromStop == stops[1] and toStop == stops[0]:
-                total += cost
-                transitRecorder.append(row)
-                print dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR §' + costString
+        if currentTotal == total:
+            invalidTransits.append(dayOfTheWeek + ' ' + dateString +': ' + fromStop + ' - ' + toStop + ': ' + 'EUR ' + costString)
 
     count += 1
 
-print total
+
+print '-----valid:-----'
+
+
+for i in validTransits:
+    print i
+
+print '-----invalid:-----'
+
+for i in invalidTransits:
+    print i
+
+print ''
+print 'total: ' + str(total)
 
 f.close()
