69f19902d15e268c0d487c2d4d6173a4748b0d2a
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-09-03 12:57:56 +0200

    Update mframes.py

--- mframes.py
+++ mframes.py
@@ -1,24 +1,39 @@
-#open terminal and type 'python mframes.py ' followed by the path to the file
+#!/Users/hanswillemgijzel/anaconda/bin/python
+
+"""returns the missing frames from an image sequnce"""
 
 import sys
 import os
 import re
 
 def main():
-    p = sys.argv[1]
+    if len(sys.argv) < 2:
+        print 'no path!'
+    else:
+        p = sys.argv[1]
 
-    #get numbers from filenames and excluding hidden files
-    frames = [int(re.findall('\d+', os.path.splitext(f)[0])[-1]) for f in os.listdir(p) if f[0] != '.']
+        try:
+            #get numbers from filenames and excluding hidden files
+            frames = [int(re.findall('\d+', os.path.splitext(f)[0])[-1]) for f in os.listdir(p) if f[0] != '.']
+            missingFrames = []
 
+        except:
+            print 'that does not seem to be a folder with an image sequence!'
+            sys.exit(0)
 
-    start = frames[0]
-    end = frames[-1]
-    print 'missing frames:'
-    for i in range(start, end + 1):
-        if i not in frames:
-            print str(i)
+        start = frames[0]
+        end = frames[-1]
+
+        for i in range(start, end + 1):
+            if i not in frames:
+                print str(i)
+                missingFrames.append(i)
+
+        if len(missingFrames) == 0:
+            print 'no missing frames!'
 
 
 
 if __name__ == '__main__':
     main()
+
