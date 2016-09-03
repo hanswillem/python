3976f771798e9b76831660982d1eac942baa25d5
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-09-03 13:30:40 +0200

    Update mframes.py

--- mframes.py
+++ mframes.py
@@ -2,8 +2,8 @@
 
 """returns the missing frames from an image sequence"""
 
-# command line application 
-# install instructions:
+# command line application install instructions Mac OSX:
+# 
 # 1. Put a shebang at the top of the script e.g. #!/usr/local/bin/python. type 'which python' in terminal if you don't know what the shebang should be.
 # 2. Save the file without '.py' extension, open terminal and type 'chmod u+x <filename>', to make the file executable.
 # 3. Copy the file to one of the PATH folders. To find PATH, type $PATH in terminal (returns multiple folders separated by ':').
