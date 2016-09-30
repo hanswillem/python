04329fcd5c359f5c2cc6f48ad69c412f5d2344a2
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2016-09-30 23:02:28 +0200

    Update and rename resize.py to createCardWithImage.py

---
+++ createCardWithImage.py
@@ -0,0 +1,52 @@
+
+"""
+Copyright: Hans Willem Gijzel (hanswillemgijzel@gmail.com)
+C4D R17.048
+
+Name-US:Create Card With Image
+Description-US:Creates a card with the selected material as texture
+"""
+
+
+import c4d,os 
+from c4d import gui, bitmaps 
+
+
+def getImageSize(m): 
+    pathToFile = m[c4d.MATERIAL_COLOR_SHADER][c4d.BITMAPSHADER_FILENAME]
+    bmp = bitmaps.BaseBitmap(pathToFile) 
+    bmp.InitWith(pathToFile)   
+    x, y = bmp.GetSize() 
+    return [x, y]
+
+
+def resizeCard(card, mat):
+    card[c4d.PRIM_PLANE_WIDTH] = getImageSize(mat)[0]
+    card[c4d.PRIM_PLANE_HEIGHT] = getImageSize(mat)[1]
+
+
+def createCard(mat):
+    card = c4d.BaseObject(5168)
+    card[c4d.PRIM_AXIS] = 5
+    card[c4d.PRIM_PLANE_SUBW] = 1
+    card[c4d.PRIM_PLANE_SUBH] = 1
+    doc.InsertObject(card)
+    texTag = c4d.BaseTag(5616)
+    texTag[c4d.TEXTURETAG_MATERIAL] = mat
+    texTag[c4d.TEXTURETAG_PROJECTION] = 6
+    card.InsertTag(texTag)
+    resizeCard(card, mat)
+    
+
+def createMultipleCards():
+    for mat in doc.GetActiveMaterials():
+        createCard(mat)
+
+
+def main():
+    createMultipleCards()
+    c4d.EventAdd()
+    
+
+if __name__ == '__main__':
+    main()
